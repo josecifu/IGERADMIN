8f2490262d38b47db68f8af6c60362336573810d
josecifu <josecifu@mesoamericana.edu.gt>
2020-09-24 17:20:40 -0600

    cambios en controllers

---
+++ app/Http/Controllers/Teacher.php
@@ -0,0 +1,18 @@
+<?php
+//Tabla de informacion
+use App\information;
+//tabla de asignacio de informacion
+use App\Assign_files_information;
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Teacher extends Controller
+{
+    public function Create_Information()
+    {
+
+    }
+}
