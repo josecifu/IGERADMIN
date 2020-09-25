0c18eaf1a941c1bb987715880f3e3a5f050ef59b
Escobar Juárez <alexander.juarez@mesoamericana.edu.gt>
2020-09-24 18:12:25 -0600

    Modelos Agregados y funciones creadas

--- app/Http/Controllers/Student.php
+++ app/Http/Controllers/Student.php
@@ -1,7 +1,11 @@
 <?php
 //Asignacion de estudiantes a grados
 use App\Assign_student_grade;
-
+use App\Asing_answer_test_student;
+use App\Asssign_schedule_course;
+use App\person;
+use App\schedule;
+use App\Note;
 
 namespace App\Http\Controllers;
 
@@ -9,8 +13,22 @@ use Illuminate\Http\Request;
 
 class Student extends Controller
 {
-    public function ViewNotes()
-    {
+    public function ViewNotes(){
+
+    }
+    public function ViewTests(){
+
+    }
+    public function AnswerTests(){
+
+    }
+    public function ViewInformationCourses(){
+
+    }
+    public function ViewSchedule(){
+
+    }
+    public function ViewProfile(){
 
     }
 }
