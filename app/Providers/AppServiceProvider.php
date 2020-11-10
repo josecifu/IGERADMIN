478812a481bdf7d58dc5fe65f323ce84ef983492
josecifu <josecifu@mesoamericana.edu.gt>
2020-11-10 16:03:31 -0600

    cambio de apprpovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
         View::composer("Administration.Base._header", function ($view) {
 
             $menus = menu::getMenu(true);
-            $view->with('Menus', $menus);
+            $view->with('menus', $menus);
         });
         View::share('Base');
     }
